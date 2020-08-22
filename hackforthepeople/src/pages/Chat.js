600e4c15a7462223a96f0425f45e2b21e04a9ff4
ingridt123 <ingridszenga@gmail.com>
2020-08-22 18:04:58 -0500

    fix meeting id for meetingDoc

--- hackforthepeople/src/pages/Chat.js
+++ hackforthepeople/src/pages/Chat.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useDocument } from 'react-firebase-hooks/firestore';
+import { db } from './../firebase';
 import ChatBox from './../components/ChatBox';
 
 const Chat = ({ user }) => {
 
-    return (
-        <ChatBox user={user} />
-    )
+    const [meetingId, setMeetingId] = useState('');
+
+    const [userDoc, userLoading, userError] = useDocument(
+        db.collection('users').doc(user.uid)
+    );
+
+    useEffect(() => {
+        if (!!userDoc) {
+            setMeetingId(userDoc.data()['currentMeeting']);
+        }
+    }, [userDoc])
+
+    if (meetingId !== '') {
+        return <ChatBox user={user} meetingId={meetingId} />;
+    }
+    return <></>
+
+    // return (
+    //     {meetingId !== '' ? <ChatBox user={user} meetingId={meetingId} /> : <></>}
+    // )
 }
 
 export default Chat;
