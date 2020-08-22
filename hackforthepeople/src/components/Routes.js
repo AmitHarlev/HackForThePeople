aa64bf48aac1f32de480b2e13ccec9616dcce71c
ingridt123 <ingridszenga@gmail.com>
2020-08-21 21:32:39 -0500

    add proper exports

--- hackforthepeople/src/components/Routes.js
+++ hackforthepeople/src/components/Routes.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Route, Redirect } from "react-router-dom";
 
+<<<<<<< HEAD
 
 const PrivateRoute = ({ component: Component, user, loading, ...rest }) => {
+=======
+const PrivateRoute = ({ component: Component, authenticated, ...rest }) => {
+>>>>>>> add proper exports
     return (
         loading ? <div/> :
         <Route
@@ -15,4 +19,21 @@ const PrivateRoute = ({ component: Component, user, loading, ...rest }) => {
     );
 }
 
+<<<<<<< HEAD
 export { PrivateRoute };
+=======
+// TODO: redirect if already completed survey
+const SurveyRoute = ({ component: Component, authenticated, ...rest }) => {
+    return (
+        <Route
+            {...rest}
+            render={(props) => authenticated == true
+                ? <Component {...props} />
+                : <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+            }
+        />
+    );
+}
+
+export default { PrivateRoute, SurveyRoute };
+>>>>>>> add proper exports
