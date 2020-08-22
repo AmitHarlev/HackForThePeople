9827c684a7930d4ede37ec86d01aafd4a7a5e0a3
ingridt123 <ingridszenga@gmail.com>
2020-08-21 21:43:09 -0500

    everything fixed with imports except routes

--- hackforthepeople/src/App.js
+++ hackforthepeople/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase';
+
 import './App.css';
 import HeaderBar from './components/HeaderBar';
 import Opinions from './components/Opinions';
-import Matcher from './components/Matcher';
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './firebase';
 import { PrivateRoute, SurveyRoute } from './components/Routes';
 import Home from './pages/Home';
-import Chat from './pages/Chat';
 import Survey from './pages/Survey';
+import Matches from './pages/Matches';
+import Chat from './pages/Chat';
 
 function App() {
   const [user, loading, error] = useAuthState(auth);
@@ -24,6 +28,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home}></Route>
           <SurveyRoute path="/survey" component={Survey}></SurveyRoute>
+          <PrivateRoute path="/matches" authenticated={user} component={Matches}></PrivateRoute>
           <PrivateRoute path="/chat" authenticated={user} component={Chat}></PrivateRoute>
         </Switch>
       </Router>
