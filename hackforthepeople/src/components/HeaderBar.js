da6f1dbd9742535a6c5ee0bab46cd700a976ef4b
Amit Harlev <aharlev@hmc.edu>
2020-08-22 22:13:18 -0700

    add nav links

--- hackforthepeople/src/components/HeaderBar.js
+++ hackforthepeople/src/components/HeaderBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Button } from 'react-bootstrap';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 import { signInWithGoogle, signOut} from '../firebase'
 
 
@@ -10,7 +10,15 @@ const HeaderBar = (props) => {
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home">Hack for the People</Navbar.Brand>
             <Navbar.Toggle />
+            {!user ? <div/> :
+            <Nav className="mr-auto">
+                <Nav.Link href="/survey">Opinions</Nav.Link>
+                <Nav.Link href="/match">Match</Nav.Link>
+                <Nav.Link href="/requests">Inbox</Nav.Link>
+            </Nav>
+            }
             <Navbar.Collapse className="justify-content-end">
+                
                 <Navbar.Text>
                     {!user ? <Button variant="outline-info" onClick={signInWithGoogle}>Log In with Google</Button>: <span>Signed in as: <a href="#login">{user.displayName}</a> <Button variant="outline-info" onClick={signOut}>Sign out</Button></span>}
                 </Navbar.Text>
