efe3b103f514957c321d67c2fb4c7ebd35396af5
Amit Harlev <aharlev@hmc.edu>
2020-08-23 00:22:13 -0700

    Added button to accept incoming requests

--- hackforthepeople/src/components/InBoundRequests.js
+++ hackforthepeople/src/components/InBoundRequests.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { db, ignoreInboundRequest } from './../firebase';
+import { db, ignoreInboundRequest, acceptIncomingRequest } from './../firebase';
 import { useDocument } from 'react-firebase-hooks/firestore';
 import { Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
 const InBoundRequests = ({ user }) => {
     const [userDoc, loading, error] = useDocument(
         db.collection('users').doc(user.uid),
     );
+
+    let history = useHistory();
 
     if (loading) {
         return <div/>
@@ -15,6 +18,14 @@ const InBoundRequests = ({ user }) => {
     const ignoreRequest = (request) => {
         ignoreInboundRequest(request);
     }
+
+    const acceptRequest = (request) => {
+        acceptIncomingRequest(request);
+        setTimeout(() => {
+            history.push('/chat');
+        }, 200);
+        
+    }
     
 
     return (
@@ -22,7 +33,7 @@ const InBoundRequests = ({ user }) => {
             <ul>
             {
                 userDoc.data().requests.map((request) => {
-                return <li>{JSON.stringify(request)} <Button onClick={() => ignoreRequest(request)}> Ignore Request </Button></li>
+                return <li>{JSON.stringify(request)} <Button onClick={() => ignoreRequest(request)}> Ignore Request </Button><Button onClick={() => acceptRequest(request)}> Accept Request </Button></li>
                 })
             }
             </ul>
