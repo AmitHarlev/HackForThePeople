30d1882b87b4098e7271c505785c177b38a37820
Amit Harlev <aharlev@hmc.edu>
2020-08-23 07:41:13 -0700

    disable send request button in matcher after clicking it

--- hackforthepeople/src/firebase.js
+++ hackforthepeople/src/firebase.js
@@ -1,7 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-import { v4 as uuidv4 } from 'uuid';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCeHaINilUoapZQrDfQP4SI-guU1QrFcdY",
