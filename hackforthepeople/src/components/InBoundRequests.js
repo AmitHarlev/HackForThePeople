4f7bfab4d4ee0e93c21658455dc23e09d0dfe90e
Amit Harlev <aharlev@hmc.edu>
2020-08-23 08:24:11 -0700

    change format in matching list

--- hackforthepeople/src/components/InBoundRequests.js
+++ hackforthepeople/src/components/InBoundRequests.js
@@ -38,7 +38,7 @@ const InBoundRequests = ({ user }) => {
                     {
                     userDoc.data().requests.map((request) => {
                     return <li>
-                        {request.name + " (" + request.topic + ": " + request.value + ") - " + request.rating + " stars"}
+                        {request.name + " (" + request.topic + ": " + request.value + ") - Rating: " + request.rating}
                         {request.state === 1 ? <Button onClick={() => history.push('/chat')}>Join Chat</Button> : <>
                         <Button onClick={() => ignoreRequest(request)} className="button-request"> Ignore Request </Button>
                         <Button onClick={() => acceptRequest(request)} className="button-request"> Accept Request </Button> </>}
