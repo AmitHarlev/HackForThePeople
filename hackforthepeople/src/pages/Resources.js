424efa27db330e739da8f57d1476785e7fe92d93
Amit Harlev <aharlev@hmc.edu>
2020-08-22 23:09:33 -0700

    Add video to resource page

--- hackforthepeople/src/pages/Resources.js
+++ hackforthepeople/src/pages/Resources.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Resources = () => {
     return (
         <div>
-            Resources
+            <iframe width="800" height="600"
+                src="https://www.youtube.com/embed/cU6BrgBg-cs">
+            </iframe>
         </div>
     )
 }
